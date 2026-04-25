4d763b557cc744e07eacb10182cd8bd2c27af8c5
EBEUSD <damiebeus1@gmail.com>
2026-04-25 00:47:44 -0300

    finalizacion analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import App from "./App";
 import "./styles/reset.css";
 import "./styles/variables.css";
 import "./styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <Analytics />
     </BrowserRouter>
   </React.StrictMode>
 );
